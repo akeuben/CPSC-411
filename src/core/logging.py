0f359121beb88736db791a02ee120b81dd68cfe4
Avery Keuben <avery1516@gmail.com>
2026-02-04 19:24:59 -0700

    Use stderr for logging warnings and errors. Limit number of warnings.

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,15 +1,24 @@
+import sys
 
-
-def log(prefix, msg, line = None):
+def log(prefix, msg, line = None, file=sys.stdout):
     if line != None:
-        msg = f"{msg} at or around line {str(line)}"
-    print(f"{prefix}: {msg}")
+        msg = f"{msg} at or near line {str(line)}"
+    print(f"{prefix}: {msg}", file=file)
+
+warningCount = 0
 
 def logWarning(msg, line = None):
-    log("warning", msg, line)
+    global warningCount
+    warningCount += 1 
+
+    log("warning", msg, line, file=sys.stderr)
+
+    if warningCount > 10:
+        logError("too many warnings", line)
 
 def logError(msg, line = None):
-    log("error", msg, line)
+    log("error", msg, line, file=sys.stderr)
+    exit(1)
 
 def logToken(type, line, attr):
     print(f"{type} @ line {line}, attr {attr}")
