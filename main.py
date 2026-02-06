a4c4f7ae9b72d2bed35c80c932c482f54eaa933a
Avery Keuben <avery1516@gmail.com>
2026-02-06 10:18:43 -0700

    Add wrapper around generated lexer

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import sys
-from src.lex.lexer import lexFromPath
+from src.lex.lexer import lexerFromPath
+from src.core.logging import logToken
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage")
         sys.exit(1)
 
-    lexFromPath(sys.argv[1])
+    lexer = lexerFromPath(sys.argv[1])
+
+    while lexer.hasNext():
+        token = lexer.lex()
+        name = lexer.tokenName(token) 
+        logToken(name, token.line, repr(token.text))
+
--- src/lex/lexer.py
+++ src/lex/lexer.py
@@ -2,27 +2,67 @@ import antlr4
 from src.generated.lex import lex
 from src.core.logging import *
 
-class WarningListener(antlr4.error.ErrorListener.ErrorListener):
+class ErrorListener(antlr4.error.ErrorListener.ErrorListener):
+    def __init__(self, callback = None):
+        self.callback = callback
+    
     def syntaxError(self, recognizer, offendingSymbol, line, col, msg, e):
-        #for now, we assume that the only possible error the parser can throw is an unknown token error,
-        logWarning("unknown char", line)
+        logWarning("a synatx error occurred, but was not handled", line)
+        callback(recognizer, offendingSymbol, line, ocl, msg, e)
 
-def lexFromPath(path):
+class Lexer:
+    def __init__(self, scanner, errorCallback = None):
+        scanner.removeErrorListeners()
+        scanner.addErrorListener(ErrorListener(errorCallback))
+
+        self.scanner = scanner
+        self.token = None 
+        self.hasToken = False
+        self.hasNextToken = True
+
+    def lex(self):
+        if self.hasToken:
+            self.hasToken = False
+            return self.token
+
+        token = self.scanner.nextToken()
+
+        if token.type == token.EOF:
+            self.hasNextToken = False
+
+        return token
+
+    def unlex(self, token):
+        self.token = token
+        self.hasToken = True
+        self.hasNextToken = True
+
+    def peek(self):
+        token = self.lex()
+        self.unlex(token)
+
+        return token
+
+    def hasNext(self):
+        return self.hasNextToken
+
+    def tokenName(self, token):
+        return self.scanner.symbolicNames[token.type] if token.type != token.EOF else "<EOF>"
+
+    
+        
+
+def lexerFromPath(path, errorCallback = None):
     # Deal with non-ascii characters by replacing with unknown char so the 
     # lexer can still find the string and report a warning.
     # From: https://docs.python.org/3/library/codecs.html
     # and:  https://github.com/antlr/antlr4/blob/8e6fd9147b3c9d36b60e2b6656871a55227efb1b/runtime/Python3/src/antlr4/FileStream.py
     stream = antlr4.FileStream(path, errors='replace')
-    lexFromStream(stream)
 
-def lexFromStream(stream):
+    return lexerFromStream(stream, errorCallback)
+
+def lexerFromStream(stream, errorCallback = None):
     scanner = lex(stream)
 
-    scanner.removeErrorListeners()
-    scanner.addErrorListener(WarningListener())
+    return Lexer(scanner, errorCallback)
 
-    while(t := scanner.nextToken()):
-        name = scanner.symbolicNames[t.type] if t.type != t.EOF else "<EOF>"
-        logToken(name, t.line, repr(t.text))
-        if t.type == t.EOF:
-            break
