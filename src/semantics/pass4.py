f264d55c4ba76233ecef741c20a7ae9eabc39560
Avery Keuben <avery1516@gmail.com>
2026-03-20 18:05:25 -0600

    fix incorrect return statement checking

--- src/semantics/pass4.py
+++ src/semantics/pass4.py
@@ -14,6 +14,7 @@ class Pass4(AstTraversal):
     table: SymbolTable
     breakDepth: int
     expectedReturnType: Type
+    foundReturn: bool;
 
     def __init__(self, ast: Ast, table: SymbolTable):
         super().__init__(ast)
@@ -33,17 +34,31 @@ class Pass4(AstTraversal):
 
     def n_funcDecl(self, node: Ast):
         self.expectedReturnType = node[0].sig
+        self.foundReturn = False
+
+    def n_funcDecl_exit(self, node: Ast):
+        retNode = node[0]
+        idNode = node[1]
+        name = idNode.attr
+
+        if not self.foundReturn and retNode.sig != TypeVoid():
+            logError(f'no return statement in non-void function {repr(name)}')
 
     def n_mainDecl(self, node: Ast):
         self.expectedReturnType = TypeVoid()
 
     def n_returnStmt(self, node: Ast):
-        if len(node) == 0 and self.expectedReturnType != TypeVoid():
-            logError("this function can't return a value", node.lineno)
-
         if len(node) == 0:
+            if self.expectedReturnType != TypeVoid():
+                logError("this function must return a value", node.lineno)
             return
+
+        if self.expectedReturnType == TypeVoid():
+            logError("this function can't return a value", node.lineno)
+        
 
         if node[0].sig != self.expectedReturnType:
             logError("returned value has the wrong type", node.lineno)
+
+        self.foundReturn = True;
 
